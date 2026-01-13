a2ce008301c04f79ff48748c14eb09b3a5d3deda
MuhammadMoaz <muhammadmoaz2020@outlook.com>
2026-01-14 01:06:37 +1100

    imported yfinance and pulled BTC data

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("this is the start of something big")
+import yfinance as yf
+import pandas as pd
+
+def fetch_data(ticker):
+    # data = yf.download(ticker, period=max, multi_level_index=False)
+    btc = yf.Ticker("BTC-USD")
+    btc_hist = btc.history(period="max")
+    fname = ticker + "_data.csv"
+    btc_hist.to_csv(fname)
+
+def main():
+    fetch_data("BTC-USD")
+
+main()
