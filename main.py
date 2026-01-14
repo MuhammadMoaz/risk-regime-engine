5a8998ce8238ffeb55e7fb612807ba43dfe25090
MuhammadMoaz <muhammadmoaz2020@outlook.com>
2026-01-14 21:08:55 +1100

    added daily returns column

--- main.py
+++ main.py
@@ -2,13 +2,30 @@ import yfinance as yf
 import pandas as pd
 
 def fetch_data(ticker):
-    # data = yf.download(ticker, period=max, multi_level_index=False)
+    print()
+
+def get_daily_returns(data):
+    print()
+    
+def main():
+    # btc_hist = fetch_data("BTC-USD")
+    # get_daily_returns(btc_hist)
+
     btc = yf.Ticker("BTC-USD")
     btc_hist = btc.history(period="max")
-    fname = ticker + "_data.csv"
-    btc_hist.to_csv(fname)
+    print(btc_hist.info)
 
-def main():
-    fetch_data("BTC-USD")
+    # Dropping empty columns (Dividends and Stock Splits)
+    btc_hist.drop(['Dividends', 'Stock Splits'], axis=1, inplace=True)
+    
+    # Calculating daily returns and inserting values as new column
+    daily_returns = btc_hist['Close'].pct_change()
+    btc_hist['Daily Returns'] = daily_returns
+
+    # Calculating 7 day rolling volatility
+    
+
+    print(btc_hist.info)
+
 
 main()
