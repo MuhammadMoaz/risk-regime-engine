012a74d24b0973489e3249a8b35175b999300956
MuhammadMoaz <muhammadmoaz2020@outlook.com>
2026-01-16 00:53:55 +1100

    start on calculating 7 day rolling volatility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import yfinance as yf
 import pandas as pd
+import numpy as np
 
 def fetch_data(ticker):
     print()
@@ -19,11 +20,17 @@ def main():
     btc_hist.drop(['Dividends', 'Stock Splits'], axis=1, inplace=True)
     
     # Calculating daily returns and inserting values as new column
-    daily_returns = btc_hist['Close'].pct_change()
+    daily_returns = (btc_hist['Close'] / btc_hist['Close'].shift(1)) - 1
     btc_hist['Daily Returns'] = daily_returns
 
     # Calculating 7 day rolling volatility
-    
+    # 1. Get logarithmic daily returns
+    log_daily_returns = np.log(btc_hist['Close'] / btc_hist['Close'].shift(1))
+    btc_hist['Log Daily Returns'] = log_daily_returns
+    # 2. Get average of returns for 7 day window
+    # 3. Calc sample stdev
+    # 4. (Optional) annualize volatility
+    # 5. Roll the window (2->8, 3->9, etc)
 
     print(btc_hist.info)
 
