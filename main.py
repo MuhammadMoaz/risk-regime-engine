9a222dcf1ddcfe35c3cfd029a05dc70096c4c603
MuhammadMoaz <muhammadmoaz2020@outlook.com>
2026-01-17 00:24:37 +1100

    calc'd 7 day rolling volatility, want to add annualised voliatility since i've read its common practice

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def main():
 
     btc = yf.Ticker("BTC-USD")
     btc_hist = btc.history(period="max")
-    print(btc_hist.info)
+    print(btc_hist.info())
 
     # Dropping empty columns (Dividends and Stock Splits)
     btc_hist.drop(['Dividends', 'Stock Splits'], axis=1, inplace=True)
@@ -34,8 +34,9 @@ def main():
     print(stdev)
     # 4. (Optional) annualize volatility
     # 5. Roll the window (2->8, 3->9, etc)
+    btc_hist['7 Day Rolling Volatility'] = btc_hist['Log Daily Returns'].rolling(window=7).std()
 
-    print(btc_hist.info)
+    print(btc_hist.tail(10))
 
 
 main()
